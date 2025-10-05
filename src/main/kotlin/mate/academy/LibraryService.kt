9a1a0d5e3550e8aeef12e3d1e753fcadf601850c
YevhenBondarenko <bondarenkojek@gmail.com>
2025-10-05 20:20:35 +0200

    implemented Books library

--- src/main/kotlin/mate/academy/LibraryService.kt
+++ src/main/kotlin/mate/academy/LibraryService.kt
@@ -1,20 +1,26 @@
 package mate.academy
 
 class LibraryService {
+    private val library = mutableListOf<Book>()
+
     fun addBook(book: Book) {
-        // TODO: implement
+        library.add(book)
     }
 
-    fun searchByTitle(title: String): List<Book> {
-        // TODO: implement
-    }
+    fun searchByTitle(title: String) = library.filter { it.title.contains(title, true) }
 
+    fun searchByAuthor(authorName: String) =
+        library.filter { it.authors.any() { it.name.contains(authorName, true) } }
 
-    fun searchByAuthor(authorName: String): List<Book> {
-        // TODO: implement
-    }
-
-    fun searchByGenre(genre: String): List<Book> {
-        // TODO: implement
-    }
+    fun searchByGenre(genre: String) = library.filter { it.genre.equals(genre, true) }
 }
+
+data class Book(
+    val title: String,
+    val authors: List<Author>,
+    val publicationYear: Int,
+    val genre: String,
+    val isbn: String
+)
+
+data class Author(val name: String)
